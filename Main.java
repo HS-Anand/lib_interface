3b983de617cdf3ccf41b0c0efbf208350a1fd018
HS-Anand <156099405+HS-Anand@users.noreply.github.com>
2025-04-20 16:01:06 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,27 @@
+package cms;
+
+import javafx.scene.Scene;
+import javafx.geometry.Pos;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+
+public class Main {
+    
+
+    public Scene getMainScene(Stage myStage) {
+        myStage.setTitle("Cattle Management System");
+
+        return null;
+    }
+}
