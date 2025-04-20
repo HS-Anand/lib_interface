e8aa2889efa0039bd0460fd23c7685759aff0f85
HS-Anand <156099405+HS-Anand@users.noreply.github.com>
2025-04-20 15:58:38 +0530

    Create manageBook.java

---
+++ manageBook.java
@@ -0,0 +1,27 @@
+package cms;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+public class manageBook {
+
+    public Scene manageBookScene(Stage stage) {
+        stage.setTitle("➕ Admin - Add New Books");
+
+
+
+        VBox dashboardBox = new VBox(50);
+        dashboardBox.setPadding(new Insets(40));
+        dashboardBox.setAlignment(Pos.TOP_CENTER);
+
+        StackPane root = new StackPane(dashboardBox);
+        root.setStyle("-fx-background-color: linear-gradient(to bottom right, #fef9f9, #dde6ed);");
+
+        return new Scene(root, 600, 600);
+    }
+}
